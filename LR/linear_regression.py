c8c2bdafa1e434eebb23786861eed0bc9299b698
Your Name <you@example.com>
2020-02-29 09:50:27 -0600

    Linear Regression Example

--- LR/linear_regression.py
+++ LR/linear_regression.py
@@ -5,15 +5,18 @@ from sklearn.linear_model import LinearRegression
 # Assign the dataframe to this variable.
 # TODO: Load the data
 bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
-x_values = bmi_life_data[['BMI']]
-y_values = bmi_life_data[['Life expectancy']]
+# x_values = bmi_life_data[['BMI']]
+# y_values = bmi_life_data[['Life expectancy']]
 
 # Make and fit the mlinear regression model
 # TODO: Fit the model and Assign it to bmi_life_model
 bmi_life_model = LinearRegression()
-bmi_life_model.fit(x_values, y_values)
+# bmi_life_model.fit(x_values, y_values)
+# bmi_life_model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
+bmi_life_model.fit(bmi_life_data[['BMI']], bmi_life_data[['LifeExpectancy']])
 
 # Mak a prediction using the model
 # TODO: Predict life expectancy for a BMI value of 21.07931
+
 laos_life_exp = bmi_life_model.predict([[ 21.07931]])
 print(laos_life_exp)
